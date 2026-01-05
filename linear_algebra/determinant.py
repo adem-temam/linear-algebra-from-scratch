39aa4167b73ba47933450d82eccef165873a9c1b
Adem Mohammed <89693312+Adem135@users.noreply.github.com>
2026-01-05 16:21:18 +0300

    Add determinant computation using row-reduction method

--- linear_algebra/determinant.py
+++ linear_algebra/determinant.py
@@ -19,3 +19,31 @@ def determinant(A):
         det += ((-1) ** col) * A[0][col] * determinant(submatrix)
 
     return det
+
+def determinant_row_reduction(A):
+    A = [row[:] for row in A]
+    n = len(A)
+    det = 1
+
+    for i in range(n):
+        pivot = i
+        while pivot < n and A[pivot][i] == 0:
+            pivot += 1
+
+        if pivot == n:
+            return 0
+
+        if pivot != i:
+            A[i], A[pivot] = A[pivot], A[i]
+            det *= -1
+
+        det *= A[i][i]
+        pivot_val = A[i][i]
+
+        for j in range(i + 1, n):
+            factor = A[j][i] / pivot_val
+            for k in range(i, n):
+                A[j][k] -= factor * A[i][k]
+
+    return det
+
